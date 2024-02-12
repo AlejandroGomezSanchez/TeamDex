a5f700f26ca288d79535fea05b94153f6e38606b
Aletornado221 <aletornado221@gmail.com>
2024-02-12 19:17:03 +0100

    DetailFavItemFragment

--- app/src/main/java/com/main/teamdex/EquipoProvider.kt
+++ app/src/main/java/com/main/teamdex/EquipoProvider.kt
@@ -3,9 +3,9 @@ package com.main.teamdex
 class EquipoProvider {
     companion object{
         var listaEquipo = mutableListOf(
-            Equipo(intArrayOf(3,14,74,23,77,43), mutableListOf<Pokemon>(),false,"Joaquin Dominguez", "Equipazo"),
-            Equipo(intArrayOf(6,18,88,31,130,61),mutableListOf<Pokemon>(),false,"Pepe Botella", "MiEquipo"),
-            Equipo(intArrayOf(9,11,63,45,120,150),mutableListOf<Pokemon>(),false,"Paco Perez", "MejorEquipo")
+            Equipo(intArrayOf(3,14,74,23,77,43), mutableListOf<Pokemon>(),false,"Joaquin Dominguez", "Equipazo",""),
+            Equipo(intArrayOf(6,18,88,31,130,61),mutableListOf<Pokemon>(),false,"Pepe Botella", "MiEquipo",""),
+            Equipo(intArrayOf(9,11,63,45,120,150),mutableListOf<Pokemon>(),false,"Paco Perez", "MejorEquipo","")
         )
 
         var listaFavEquipo = mutableListOf<Equipo>()
@@ -33,7 +33,10 @@ class EquipoProvider {
         }
 
         public fun resetFav(){
-            listaFavEquipo.clear()
+            listaEquipo.forEach {
+                it.fav=false;
+                it.anotacion=""
+            }
         }
     }
 }
